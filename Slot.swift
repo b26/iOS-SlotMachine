5999e8e9df4c05b028fe4f970f4a64d0312dca21
Bashir Osman <bashir@b26.co>
2016-03-07 10:55:49 -0700

    final commit

---
+++ Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Bashir on 2015-01-17.
+//  Copyright (c) 2015 bashir. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
---
+++ SlotMachine/test.playground/section-1.swift
@@ -0,0 +1,16 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+//var str = "Hello, playground"
+//
+//var random = Int(arc4random_uniform(UInt32(32)))
+//println(random)
+//
+//func rerun(x:Int) -> Int {
+//    return x
+//}
+//
+//rerun(random)
+
+var array:[Bool]
